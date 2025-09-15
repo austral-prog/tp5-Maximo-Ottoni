11fb8e852bea2ef97bc7e78ba3b81987c48dcfc0
Maximo-Ottoni <mtottoni@mail.austral.edu.ar>
2025-09-15 09:12:01 -0300

    Add files via upload

---
+++ maximums (1).py
@@ -0,0 +1,6 @@
+def max_of_two(x, y):
+    return max(x, y)
+print(max_of_two(-1, 3))
+def max_of_three(x, y, z):
+    return max(x, y, z)
+print(max_of_three(-1, 78, 2))
---
+++ number_to_month (1).py
@@ -0,0 +1,8 @@
+def number_to_month(month):
+    months = ["enero", "febrero", "marzo", "abril", "mayo", "junio",
+              "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"]
+    if 1 <= month <= 12:
+        return months[month - 1]
+    else:
+        return "error"
+print(number_to_month(0))
